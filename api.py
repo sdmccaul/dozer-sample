c6c8a4a7ce9a9582131610cba099843790286273
Steve McCauley <steven_mccauley@brown.edu>
2016-11-04 09:11:53 -0400

    Add API for vocabulary management

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 #!flask/bin/python
 from flask import Flask, jsonify, abort, request, make_response, url_for
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app, expose_headers='ETag')
 app.config.from_pyfile('config/api.cfg')
 
 os.environ['QUERY_URL'] = app.config['QUERY_URL']
@@ -267,5 +269,87 @@ def destroy_fis_orgs(rabid):
 						status_code=409,
 						payload=fisorg.to_dict())
 
+## API for R@B Vocabulary data
+from resources.vocab import Terms
+
+@app.route('/vocab/', methods=['GET'])
+def index_vocab_terms():
+	# Working for single strings
+	# problems for dates, multival?
+	params = { k: [v] for k, v in request.args.items() }
+	try:
+		allTerms = Terms.search(params=params)
+	except AliasError as e:
+		raise RESTError('Bad parameter',
+			status_code=400, payload=e.msg)
+	except:
+		raise RESTError('Resource not found', status_code=404)
+	return json.dumps([ term.to_dict()
+							for term in allTerms])
+
+@app.route('/vocab/<rabid>', methods=['GET'])
+def retrieve_vocab_term(rabid):
+	try:
+		term = Terms.find(rabid=rabid)
+	except:
+		raise RESTError('Resource not found', status_code=404)
+	resp = make_response(
+				json.dumps(term.to_dict()))
+	resp.headers['ETag'] = term.etag
+	return resp
+
+@app.route('/vocab/', methods=['POST'])
+def create_vocab_term():
+	try:
+		term = Terms.create(
+					data=request.get_json())
+	except (AliasError, ValidationError) as e:
+		raise RESTError('Bad data',
+					status_code=400, payload=e.msg) 
+	resp = make_response(
+				json.dumps(term.to_dict()))
+	resp.headers['ETag'] = term.etag
+	return resp
+
+@app.route('/vocab/<rabid>', methods=['PUT'])
+def replace_vocab_term(rabid):
+	try:
+		term = Terms.find(rabid=rabid)
+	except:
+		raise RESTError('Resource not found', status_code=404)
+	if term.etag == request.headers.get("If-Match"):
+		try:
+			updated = Terms.overwrite(
+						term, request.get_json())
+		except (AliasError, ValidationError) as e:
+			raise RESTError('Validation',
+				status_code=400, payload=e.msg)
+		resp = make_response(
+				json.dumps(updated.to_dict()))
+		resp.headers['ETag'] = updated.etag
+		return resp
+	else:
+		raise RESTError('Data modified on server',
+						status_code=409, payload=term.to_dict())
+
+@app.route('/vocab/<rabid>', methods=['DELETE'])
+def destroy_vocab_term(rabid):
+	try:
+		term = Terms.find(rabid=rabid)
+	except:
+		raise RESTError('No resource to delete', status_code=410)
+	if term.etag == request.headers.get("If-Match"):
+		try:
+			Terms.remove(term)
+			resp = make_response('', 204)
+			return resp
+		except (AliasError, ValidationError) as e:
+			raise RESTError('Validation',
+				status_code=400, payload=e.msg)
+	else:
+		raise RESTError('Data modified on server',
+						status_code=409,
+						payload=term.to_dict())
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0',port=8000)
